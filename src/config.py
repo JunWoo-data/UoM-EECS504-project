2039f54552732fecd2b2e90fb9a63226053b9384
root <yooungjun.woo.data@gmail.com>
2022-12-04 21:41:42 +0000

    update config and dataset

--- src/config.py
+++ src/config.py
@@ -1,8 +1,16 @@
+# %%
+import torch
+
+# %%
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
 ## utils.py
 GAUSSIAN_KERNEL_SIZE = 20
 GAUSSIAN_KERNEL_VARIANCE = 10
 
 ## datasets.py
 NUM_CLIP = 95
-CLIP_LABEL_PATH = "/content/drive/My Drive/eecs504/project/data/dataset/clip/"
+CLIP_PATH = "/content/drive/My Drive/eecs504/project/data/dataset/clip/"
 GROUND_TRUTH_PATH = "/content/drive/My Drive/eecs504/project/data/dataset/ground_truth/"
+
+# %%
--- src/datasets.py
+++ src/datasets.py
@@ -3,3 +3,8 @@ from config import NUM_CLIP, GAUSSIAN_KERNEL_VARIANCE
 
 
 # %%
+from google.colab import drive
+
+# %%
+drive.mount("/content/drive")
+# %%
