40d420e8de535140a2f6424cf82d3eac8673c604
root <youngjun.woo.data@gmail.com>
2022-12-16 22:02:09 +0000

    add output, saved state path

--- src/config.py
+++ src/config.py
@@ -22,3 +22,7 @@ NUM_CLIP = 95
 DATA_PATH = "/content/drive/My Drive/eecs504/project/data/"
 FRAME_LABEL_PATH = "/content/drive/My Drive/eecs504/project/data/dataset/frame_label/"
 GT_HEATMAP_PATH = "/content/drive/My Drive/eecs504/project/data/dataset/ground_truth_heatmap/"
+
+## engine.py
+SAVED_STATE_PATH = "/content/drive/My Drive/eecs504/project/saved_states/"
+OUTPUT_PATH = '/content/drive/My Drive/eecs504/project/output/'
